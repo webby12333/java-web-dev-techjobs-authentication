52b60aa3f3f7d261ed0ca29755f567d235e7a321
webby12333 <webby12333@yahoo.com>
2020-08-02 19:10:47 -0500

    Testing

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
@@ -0,0 +1,54 @@
+package org.launchcode.javawebdevtechjobsauthentication;
+
+import org.launchcode.javawebdevtechjobsauthentication.controllers.AuthenticationController;
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    // PreHandle Method
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+        if (isWhitelisted(request.getRequestURI())) {
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+        if (user != null) {
+            return true;
+        }
+
+        response.sendRedirect("/login");
+        return false;
+    }
+
+}
---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/WebApplicationConfig.java
@@ -0,0 +1,19 @@
+package org.launchcode.javawebdevtechjobsauthentication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor( authenticationFilter() );
+    }
+
+}
