4f55bbd26e3f3d52582ce7289e5401cbb548f9e4
webby12333 <webby12333@yahoo.com>
2020-08-02 19:53:21 -0500

    Completed

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepositor
 import org.launchcode.javawebdevtechjobsauthentication.models.dto.LoginFormDTO;
 import org.launchcode.javawebdevtechjobsauthentication.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Optional;
 
+@Controller
 public class AuthenticationController {
 
     @Autowired
